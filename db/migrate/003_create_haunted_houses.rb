dac4d42b6da5860560e098768dc70be55f23c99f
Joseph Sesti <sestijoey63@gmail.com>
2019-10-31 19:46:10 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,7 +8,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       h.string :price
       h.boolean :family_friendly
       h.datetime :opening_date
-      h.datetime :closing_time
+      h.datetime :closing_date
       h.string :description
     end
   end
